9edc859dbfb484291ecf3e71a94f4863479131ff
admin <admin@Marks-mac-mini.local>
2017-05-15 17:33:53 +0900

    Add mac drivers

--- kobis.or.kr/AllMovies_kobis.py
+++ kobis.or.kr/AllMovies_kobis.py
@@ -17,30 +17,13 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import logging  
 import ssl
-import Config
-logging.basicConfig(filename=Config.LOG_PATH,level=logging.DEBUG)
 
 ssl._create_default_https_context = ssl._create_unverified_context
-os.environ["PARSE_API_ROOT"] = Config.PARSE_SERVER_URI
 
-from parse_rest.datatypes import Function, Object, GeoPoint, File
-from parse_rest.connection import register
-from parse_rest.query import QueryResourceDoesNotExist
-from parse_rest.connection import ParseBatcher
-from parse_rest.core import ResourceRequestBadRequest, ParseError
 
-register(Config.APPLICATION_ID, "", master_key=Config.MASTER_KEY)
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
-# for saving server progress to backend
-class Parsing(Object):
-    pass
-
-class Parsing_err(Object):
-    pass
 
 # currError = Parsing_err(page_num=0, entity_num=1, err_url="test", status="pending")
 # currError.save()
