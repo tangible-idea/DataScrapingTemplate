dfd133a8a059ed03b37c3ca84484bd0d6794a78e
admin <admin@Marks-mac-mini.local>
2017-05-16 02:17:47 +0900

    Try to find international table

--- boxofficemojo.com/Box Office Mojo_Scraping code.py
+++ boxofficemojo.com/Box Office Mojo_Scraping code.py
@@ -4,76 +4,86 @@ from bs4 import BeautifulSoup
 import logging  
 logging.basicConfig(level=logging.DEBUG)
 
-def save_to_file(filePath, data):
+def add_empty_data(arrData, count):
+    for i in range(0,count):
+        arrData.append(" ")
+    return arrData
+
+def save_to_file(filePath, arrData):
     text_file = open(filePath, "a")
-    #for data in arrData:
-    #    text_file.write(str(data) + "|")
-    text_file.write(str(data))
+    for data in arrData:
+        text_file.write(str(data) + "|")
+    #text_file.write(str(data))
     text_file.write('\n')
     text_file.close()
 
+def get_movie_foreign(link, arrData):
 
-def get_movie_detail(movies_list, link):
-    # next_td_tag = row.td.findNext('td')
-    # studio = next_td_tag.font.contents[0]
+    try:
+        each_column=""
+        url = "http://www.boxofficemojo.com"+ link + "&page=intl"
+        page = urlopen(url)
+        soup = BeautifulSoup(page, "lxml")
 
-    # next_td_tag = row.td.findNext('td').findNext('td')
-    # gross = next_td_tag.font.contents[0]
+        contents= soup.find('table', attrs={'border': '3' , 'cellspacing':'0', 'cellpadding':'5', 'align':'center', 'style':'margin-top: 5px;'})
+        if len(contents) == 1:
+            print(contents)
+            intl_table= contents.tr.td.table
+            if intl_table:
+                trs= intl_table.find_all("tr")
+                if len(trs) == 3:
+                    print ("no data")
+                else:
+                    for tbl in intl_table:
+                        arrData.append(tbl.text)
+        return arrData
+    except Exception as e:
+        logging.exception(e)
 
-    # next_td_tag = row.td.findNext('td').findNext('td').findNext('td')
-    # gross_th = next_td_tag.font.contents[0]
 
-    # next_td_tag = row.td.findNext('td').findNext('td').findNext('td').findNext('td')
-    # opening = next_td_tag.font.contents[0]
+def get_movie_detail(movies_list, link, arrData):
 
-    # next_td_tag = row.td.findNext('td').findNext('td').findNext('td').findNext('td').findNext('td')
-    # opening_th = next_td_tag.font.contents[0]
-
-    # next_td_tag = row.td.findNext('td').findNext('td').findNext('td').findNext('td').findNext('td').findNext('td')                            
-    # if next_td_tag.font.a is not NoneType:
-    #     opendate = next_td_tag.font.a.getText()
-
-    
-    result= ""
+    #result= ""
     if link not in movies_list:
         #print link + '|' +  studio# + '|' + gross + '|' + gross_th+ '|' + opening + '|' + opening_th+ '|' + opendate
         #print link
         movies_list.append(link)
 
-        url2 = "http://www.boxofficemojo.com"+ link
-        page2 = urlopen(url2)
-        soup2 = BeautifulSoup(page2, "lxml")
-        #contents2 = soup2.find(id="body").find("table").find("table").find("table").find("table").find_all("td")
-        #print soup2.find(id="body").find_all("table")[1]
-        contents2= soup2.find('table', attrs={'border': '0' , 'cellspacing':'1', 'cellpadding':'4' , 'bgcolor':'#dcdcdc', 'width':'95%'})
-        tabledata2= contents2.find_all("td")
+        url = "http://www.boxofficemojo.com"+ link
+        page = urlopen(url)
+        soup = BeautifulSoup(page, "lxml")
+        #contents = soup.find(id="body").find("table").find("table").find("table").find("table").find_all("td")
+        #print soup.find(id="body").find_all("table")[1]
+        contents= soup.find('table', attrs={'border': '0' , 'cellspacing':'1', 'cellpadding':'4' , 'bgcolor':'#dcdcdc', 'width':'95%'})
+        tabledata= contents.find_all("td")
 
-        name_table2= soup2.find('table', attrs={'border': '0' , 'cellspacing':'0', 'cellpadding':'0' , 'width':'100%', 'style':'padding-top: 5px;'})
-        name2= name_table2.font.b.getText()
-        #print name2
+        name_table= soup.find('table', attrs={'border': '0' , 'cellspacing':'0', 'cellpadding':'0' , 'width':'100%', 'style':'padding-top: 5px;'})
+        name= name_table.font.b.getText()
+        #print name
         
-        if len(tabledata2) == 6:
-            Distributor= tabledata2[0].b.getText()
-            ReleaseDate= tabledata2[1].b.getText()
-            Genre = tabledata2[2].b.getText()
-            Runtime= tabledata2[3].b.getText()
-            Rating = tabledata2[4].b.getText()
-            Budget = tabledata2[5].b.getText()
-            result += name2 +'|'+ url2 +'|'+ Distributor+'|'+ ReleaseDate+'|'+Genre +'|'+Runtime +'|'+ Rating+'|'+Budget
-        elif len(tabledata2) == 7:
-            TotalGross = tabledata2[0].b.getText()
-            Distributor= tabledata2[1].b.getText()
-            ReleaseDate= tabledata2[2].b.getText()
-            Genre = tabledata2[3].b.getText()
-            Runtime= tabledata2[4].b.getText()
-            Rating = tabledata2[5].b.getText()
-            Budget = tabledata2[6].b.getText()
-            result += name2 +'|'+ url2 +'|'+ Distributor+'|'+ ReleaseDate+'|'+Genre +'|'+Runtime +'|'+ Rating+'|'+Budget +'|'+TotalGross
+        if len(tabledata) == 6:
+            Distributor= tabledata[0].b.getText()
+            ReleaseDate= tabledata[1].b.getText()
+            Genre = tabledata[2].b.getText()
+            Runtime= tabledata[3].b.getText()
+            Rating = tabledata[4].b.getText()
+            Budget = tabledata[5].b.getText()
+            arrData.extend([name , url , Distributor, ReleaseDate,Genre ,Runtime , Rating,Budget])
+        elif len(tabledata) == 7:
+            TotalGross = tabledata[0].b.getText()
+            Distributor= tabledata[1].b.getText()
+            ReleaseDate= tabledata[2].b.getText()
+            Genre = tabledata[3].b.getText()
+            Runtime= tabledata[4].b.getText()
+            Rating = tabledata[5].b.getText()
+            Budget = tabledata[6].b.getText()
+            arrData.extend([ name , url , Distributor, ReleaseDate,Genre ,Runtime , Rating,Budget ,TotalGross])
 
             #print (result)
-            save_to_file("./boxofficemojo.com/movie_data.txt", result)
+            
 
         #print contents2[0]
+        return arrData
         
 def get_all_movies():  
     """ returns all the movie urls from boxofficemojo.com in a list"""
@@ -84,6 +94,9 @@ def get_all_movies():
 
     # List of movie urls
     movies_list = []
+
+    # data
+    arrData = []
 
     # Loop through the pages for each letter
     for letter in index:
@@ -106,8 +119,9 @@ def get_all_movies():
                         # skip index row
                         if counter > 1:
                             link = row.td.font.a['href']
-                            get_movie_detail(movies_list, link)                           
-
+                            arrData= get_movie_detail(movies_list, link, arrData) 
+                            arrData= get_movie_foreign(link, arrData)
+                            save_to_file("./boxofficemojo.com/movie_data.txt", arrData)
                         counter += 1
             except Exception as e:
                 logging.exception(e)
