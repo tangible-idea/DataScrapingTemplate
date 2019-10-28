622bbfad06bc99340cde2b0e00d43cd46eb63ed6
Junhyeok Choi <fantasysa@gmail.com>
2019-10-28 11:13:57 +0900

    M: time comparision.

--- workinghour/workinghour2.py
+++ workinghour/workinghour2.py
@@ -18,17 +18,28 @@ for x in f:
   	continue
   else:
   	history_list.append(x.strip())
-  	print(x.strip())
+  	#print(x.strip())
 
 history_thismonth = []
 for item in history_list:
     #text= str(item)
-    print(item)
+    #print(item)
     if "October" in item:
        history_thismonth.append(item)
        #print(item)
 
-for t in history_thismonth:
+history_thismonth_redundant = list(dict.fromkeys(history_thismonth))
+
+arr_datetime = []
+prev_datetime = ""
+for t in history_thismonth_redundant:
     t = t + ", 2019"
+    prev_datetime
     datetime_object = datetime.strptime(t, "%B %d, %I:%M %p, %Y")
-    print(datetime_object)
+    #print(datetime_object)
+    arr_datetime.append(datetime_object)
+
+
+for i in range(len(arr_datetime)-1, 0, -1):
+	test= arr_datetime[i-1] - arr_datetime[i]
+	print(str(arr_datetime[i-1]) + " - " + str(arr_datetime[i]) + " = " + str(test))
